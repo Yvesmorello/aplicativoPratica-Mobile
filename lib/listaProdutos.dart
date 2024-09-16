77b2cbac45ac5896a9d9d911f956a5721cc9f683
Yvesmorello <124888523+Yvesmorello@users.noreply.github.com>
2024-09-16 13:01:31 -0300

    Update listaProdutos.dart

--- lib/listaProdutos.dart
+++ lib/listaProdutos.dart
@@ -6,7 +6,7 @@ class ListaProdutos extends StatelessWidget {
 
   ListaProdutos({required this.produtos});
 
-  Color _getBackgroundColor(String tipo) {
+  Color getBackgroundColor(String tipo) {
     switch (tipo) {
       case 'Ação':
         return Colors.blue[50]!; 
@@ -29,7 +29,7 @@ class ListaProdutos extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
             child: Container(
               decoration: BoxDecoration(
-                color: _getBackgroundColor(produto.categoria.nome), 
+                color: getBackgroundColor(produto.categoria.nome), 
                 borderRadius: BorderRadius.circular(10),
                 boxShadow: [
                   BoxShadow(
